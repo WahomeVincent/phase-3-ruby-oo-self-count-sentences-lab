67994e377ab35acfc8f11574fe776869fa2a187d
Wahome <vinwahome@gmail.com>
2023-05-18 19:29:49 +0300

    Completed Assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/[.!?]/)
+    count = sentences.size
+    return count
   end
 end
+
+my_string = "This is a string! It has three sentences. Right?"
+sentence_count = my_string.count_sentences
+puts sentence_count  
